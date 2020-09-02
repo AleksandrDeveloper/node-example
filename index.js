502f9b34848eb5b3a416f483bcb70fe6bb83495f
AleksandrDeveloper <aleks777.chernof7@mail.ru>
2020-09-02 22:53:29 +0300

    init git

---
+++ index.js
@@ -0,0 +1,5 @@
+const user = require('./user')
+
+console.log(__dirname,'----',__filename);
+console.log('         ');
+console.log(user);  
---
+++ user.js
@@ -0,0 +1,6 @@
+
+
+module.exports={
+    name:'Tony',
+    age:33
+}
